bb7f1163fcf0d846f6edec1ff58c406198b0b12c
Oneul <toda1213@naver.com>
2022-09-06 11:44:23 +0900

    BOJ 1240번 메모리 초과

--- src/main/kotlin/boj/1240.kt
+++ src/main/kotlin/boj/1240.kt
@@ -9,6 +9,7 @@
  * 출력
  * 1. M개의 노드쌍별 거리
  */
+// TODO : 메모리 초과 해결 (고차함수 때문인 듯)
 package boj
 
 fun main() {
@@ -33,16 +34,16 @@ fun main() {
         searchList.add(pair)
     }
 
-    // visited 를 이용한 BFS
+    // 모든 케이스에 대한 dfs
     for (pair in searchList) {
-        println(bfs(pair[0], pair[1], adjArr))
+        println(dfs(pair[0], pair[1], adjArr))
     }
 }
 
-fun bfs(start: Int, end: Int, adjArr: Array<IntArray>): Int {
-    var distance = 0
-    val q = ArrayDeque<Pair<Int, Int>>()
-    val n = adjArr.size
+// visited 를 이용한 stack 기반 dfs
+fun dfs(start: Int, end: Int, adjArr: Array<IntArray>): Int {
+    val stack = ArrayDeque<Pair<Int, Int>>()
+    val n = adjArr.size - 1
 
     // 탐색용 visited 행렬 생성 및 초기화
     val visited = Array(n + 1) { BooleanArray(n + 1) { true } }
@@ -53,7 +54,33 @@ fun bfs(start: Int, end: Int, adjArr: Array<IntArray>): Int {
         }
     }
 
-    // TODO : visited 보고 q에 담으면서 탐색 및 거리 계산
+    // stack 에 시작값 넣기
+    for ((index, isVisited) in visited[start].withIndex().reversed()) {
+        if (!isVisited) {
+            if (index == end) return adjArr[start][index]
 
-    return distance
+            stack.add(Pair(index, adjArr[start][index]))
+            visited[start][index] = true
+            visited[index][start] = true
+        }
+    }
+
+    // visited 보고 stack 에 담으면서 탐색 및 거리 계산
+    while (stack.isNotEmpty()) {
+        val pair = stack.removeLast()
+        val (node, dist) = pair
+
+        // node 에 연결된 다른 노드 탐색
+        for ((index, isVisited) in visited[node].withIndex().reversed()) {
+            if (!isVisited) {
+                if (index == end) return dist + adjArr[node][index]
+
+                stack.add(Pair(index, dist + adjArr[node][index]))
+                visited[node][index] = true
+                visited[index][node] = true
+            }
+        }
+    }
+
+    return -1
 }
