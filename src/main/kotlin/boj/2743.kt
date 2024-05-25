9856c34dbce66202231aee7d77e6b8f1dde66cb5
Oneul1213 <toda1213@naver.com>
2024-05-25 18:34:22 +0900

    백준 2743(단계별로 풀어보기)

---
+++ src/main/kotlin/boj/2743.kt
@@ -0,0 +1,18 @@
+/**
+ * 백준 2743번 - 단어 길이 재기
+ * 알고리즘 분류 - 구현, 문자열
+ *
+ * 입력
+ * 1. 영어 소문자와 대문자로만 이루어진 단어
+ *
+ * 출력
+ * 1. 입력으로 주어진 단어의 길이
+ */
+package boj
+
+fun main() {
+    val reader = System.`in`.bufferedReader()
+    val word = reader.readLine()
+
+    println(word.length)
+}
